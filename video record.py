a3d684590d3a6caf776f727478baab31ed2b11c5
Reef Horvitz <reefhorvitz@gmail.com>
2017-02-28 09:13:01 +0200

    fix self capture

--- video record.py
+++ video record.py
@@ -2,7 +2,7 @@ import cv2
 import socket
 import numpy
 import time
-TCP_IP = '127.0.0.1'
+TCP_IP = '192.168.30.38'
 TCP_PORT = 5001
 
 sock = socket.socket()
@@ -11,12 +11,14 @@ sock.connect((TCP_IP, TCP_PORT))
 capture = cv2.VideoCapture(0)
 # for i in range(0, 30):
 while 1:
-
+#capture video
     ret, frame = capture.read()
-
 
     cv2.imshow('frame',frame)
     cv2.moveWindow("frame",-15,528)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
     encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 90]
     result, imgencode = cv2.imencode('.jpg', frame, encode_param)
     data = numpy.array(imgencode)
