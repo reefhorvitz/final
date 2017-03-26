5d78064716e06356353cd2e1da4cd2e4c7053395
Dekel Yigal <dekelyi@gmail.com>
2017-03-26 13:27:16 +0300

    exit to only

--- client_only.py
+++ client_only.py
@@ -1,11 +1,13 @@
 import socket
 import Only_Video
 import multiprocessing
+import sys
+import time
 
 def Sock_Connect():
 	PORT1 = 5001
 	PORT2 = 5002
-	IP = "127.0.0.1"
+	IP = "192.168.30.20"
 	s1 = socket.socket()
 	s2 = socket.socket()
 	s1.connect((IP,PORT1))
@@ -14,9 +16,20 @@ def Sock_Connect():
 send, recv = Sock_Connect()
 base = Only_Video.Video()
 
+def Exit(P1,P2,send,recv):
+	while True:
+		if not(P1.is_alive() and P2.is_alive()):
+			P1.Terminate()
+			P2.Terminate()
+			send.close()
+			recv.close()
+			sys.exit(0)
+		time.sleep(0.5)
+
 Proc_Send = multiprocessing.Process(target=base.Get_Self_Img,args=(send,))
 Proc_Recv = multiprocessing.Process(target=base.Recv_Data,args=(recv,))
 Proc_Send.run()
 Proc_Recv.run()
 
+Exit(Proc_Recv,Proc_Send,send,recv)
 
--- server_only.py
+++ server_only.py
@@ -1,6 +1,8 @@
 import socket
 import Only_Video
 import multiprocessing
+import sys
+import time
 
 def Sock_Connect():
 	PORT1 = 5001
@@ -17,10 +19,21 @@ def Sock_Connect():
 	send_sock, addr = s2.accept()
 	return (send_sock,recv_sock)
 
+def Exit(P1,P2,send,recv):
+	while True:
+		if not(P1.is_alive() and P2.is_alive()):
+			P1.Terminate()
+			P2.Terminate()
+			send.close()
+			recv.close()
+			sys.exit(0)
+		time.sleep(0.5)
+
 send, recv = Sock_Connect()
 base = Only_Video.Video()
 
-Proc_Send = multiprocessing.Process(target=base.Get_Self_Img,args=(send,))
-Proc_Recv = multiprocessing.Process(target=base.Recv_Data,args=(recv,))
+Proc_Send = multiprocessing.Process(target=base.Get_Self_Img, args=(send,))
+Proc_Recv = multiprocessing.Process(target=base.Recv_Data, args=(recv,))
 Proc_Send.run()
 Proc_Recv.run()
+Exit(Proc_Recv, Proc_Send, send, recv)
