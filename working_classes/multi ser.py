6ca31e067801927720a30aaa6de0e4958a216076
Dekel Yigal <dekelyi@gmail.com>
2017-05-09 14:18:07 +0300

    multiproccesing

--- working_classes/multi ser.py
+++ working_classes/multi ser.py
@@ -72,29 +72,29 @@ def Exit(P1,P2,P3,P4,P5,P6,s1,s2,s3,s4,s5,s6):
 			sys.exit(0)
 		time.sleep(0.5)
 
+if __name__ == "__main__":
 
+	send_vid, recv_vid, send_aud, recv_aud, send_chat, recv_chat = Sock_Connect()
+	base = Only_Video.Video()
 
-send_vid, recv_vid, send_aud, recv_aud, send_chat, recv_chat = Sock_Connect()
-base = Only_Video.Video()
+	audio = sound_class.OnlyAudio(send_aud)
+	audio2 = sound_class.OnlyAudio(recv_aud)
 
-audio = sound_class.OnlyAudio(send_aud)
-audio2 = sound_class.OnlyAudio(recv_aud)
+	chat =  class_chat.Chat(send_chat)
+	chat2 = class_chat.Chat(recv_chat)
 
-chat =  class_chat.Chat(send_chat)
-chat2 = class_chat.Chat(recv_chat)
+	Send_Vid_Thread = multiprocessing.Process(target=base.Get_Self_Img, args=(send_vid,))
+	Recv_Vid_Thread = multiprocessing.Process(target=base.Recv_Data, args=(recv_vid,))
+	Send_Aud_Thread = multiprocessing.Process(target=audio.Send_Sound)
+	Recv_Aud_Thread = multiprocessing.Process(target=audio2.Get_Sound)
+	Send_Chat_Thread =multiprocessing.Process(target=chat.send_msg)
+	Recv_Chat_Thread =multiprocessing.Process(target=chat.recv_msg)
+	
+	Send_Vid_Thread.start()
+	Recv_Vid_Thread.start()
+	Send_Aud_Thread.start()
+	Recv_Aud_Thread.start()
+	Send_Chat_Thread.start()
+	Recv_Chat_Thread.start()
 
-Send_Vid_Thread = multiprocessing.Process(target=base.Get_Self_Img, args=(send_vid,))
-Recv_Vid_Thread = multiprocessing.Process(target=base.Recv_Data, args=(recv_vid,))
-Send_Aud_Thread = multiprocessing.Process(target=audio.Send_Sound)
-Recv_Aud_Thread = multiprocessing.Process(target=audio2.Get_Sound)
-Send_Chat_Thread =multiprocessing.Process(target=chat.send_msg)
-Recv_Chat_Thread =multiprocessing.Process(target=chat.recv_msg)
-
-Send_Vid_Thread.start()
-Recv_Vid_Thread.start()
-Send_Aud_Thread.start()
-Recv_Aud_Thread.start()
-Send_Chat_Thread.start()
-Recv_Chat_Thread.start()
-
-Exit(Send_Vid_Thread, Recv_Vid_Thread, Send_Aud_Thread, Recv_Aud_Thread, Send_Chat_Thread, Recv_Chat_Thread, send_aud, recv_aud, send_vid, recv_vid, send_chat, recv_chat)
+	Exit(Send_Vid_Thread, Recv_Vid_Thread, Send_Aud_Thread, Recv_Aud_Thread, Send_Chat_Thread, Recv_Chat_Thread, send_aud, recv_aud, send_vid, recv_vid, send_chat, recv_chat)
