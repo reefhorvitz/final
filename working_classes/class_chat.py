04958078c43b172170b7622b4ab3d074bb87c1f3
Dekel Yigal <dekelyi@gmail.com>
2017-05-14 11:09:43 +0300

    gui chat try

--- working_classes/class_chat.py
+++ working_classes/class_chat.py
@@ -1,13 +1,28 @@
 import socket
-import Tkinter
+from Tkinter import *
 
 class Chat():
 
+	def __init__(self):
+		self.root = Tk()
+		self.myentery = Entry(self.root)
+		self.myentery.pack(side = BOTTOM)
+		self.chattext = Text(self.root)
+		self.sendbutton = Button(self.root,text = "SEND" , command = self.pressed)
+		self.is_pressed = False
+
+	def pressed(self):
+		self.is_pressed = True
+
 	def recv_msg(self,s):
 		while True:
-			print s.recv(1024)
+			data = s.recv(1024)
+			if data:
+				self.chattext.insert(END,"HIM : "+data)
 
 	def send_msg(self,s):
-		while True:
-			data = raw_input()
-			s.send(data)
+		if self.is_pressed:
+			msg = self.myentery.get()
+			s.send(msg)
+			self.chattext.insert(END,"ME : "+msg)
+			self.is_pressed = False
