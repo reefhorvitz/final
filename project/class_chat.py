daad35972816143be2c7a11bf7e4f937587d7ca5
Your Name <you@example.com>
2017-05-16 20:39:24 +0300

    chat improved

--- project/class_chat.py
+++ project/class_chat.py
@@ -8,43 +8,49 @@ HEIGHT = GetSystemMetrics(1)
 
 class Chat():
 
-	def __init__(self):
-		self.root = Tk()
-		self.root.geometry('%dx%d+%d+%d'%(WIDTH-600, HEIGHT, 600, 0))
-		self.root.bind("<Return>",self.pressed)
+    def __init__(self):
+        self.root = Tk()
+        self.root.geometry('%dx%d+%d+%d'%(WIDTH-600, HEIGHT, 600, 0))
+        self.root["bg"] = "#f2ea54"
+        self.root.bind("<Return>",self.pressed)
 
-		self.chattext = Text(self.root, font = 24)
-		self.chattext.pack(side = RIGHT)
+        self.chattext = Text(self.root, font = 28)
+        self.chattext.config(stat = DISABLED)
+        self.chattext.pack(side = RIGHT)
 
-		self.myentery = Entry(self.root)
-		self.myentery.pack(side = RIGHT)
+        self.myentery = Entry(self.root)
+        self.myentery.pack(side = RIGHT)
 
-		self.sendbutton = Button(self.root,text = "SEND" , command = self.pressed)
-		self.sendbutton.pack(side = RIGHT)
+        self.sendbutton = Button(self.root,text = "SEND" , command = self.pressed)
+        self.sendbutton.pack(side = RIGHT)
 
-		self.is_pressed = False
+        self.is_pressed = False
 
-		#for the main loop
-		thread = threading.Thread(target=self.mainloop)
-		thread.start()
+        #for the main loop
+        thread = threading.Thread(target=self.mainloop)
+        thread.start()
 
-	def pressed(self,ret = None):
-		self.is_pressed = True
+    def pressed(self,ret = None):
+        self.is_pressed = True
 
-	def recv_msg(self,s):
-		while True:
-			data = s.recv(1024)
-			if data:
-				self.chattext.insert(END,"HIM : "+data+"\n")
+    def recv_msg(self,s):
+        while True:
+            data = s.recv(1024)
+            if data:
+                self.chattext.config(stat=NORMAL)
+                self.chattext.insert(END,"HIM : "+data+"\n")
+                self.chattext.config(stat=DISABLED)
 
-	def send_msg(self,s):
-		while True:
-			if self.is_pressed:
-				msg = self.myentery.get()
-				s.send(msg)
-				self.chattext.insert(END,"ME : "+msg+"\n")
-				self.is_pressed = False
-				self.myentery.delete(0,END)
+    def send_msg(self,s):
+        while True:
+            if self.is_pressed:
+                msg = self.myentery.get()
+                s.send(msg)
+                self.chattext.config(stat=NORMAL)
+                self.chattext.insert(END,"ME : "+msg+"\n")
+                self.chattext.config(stat=DISABLED)
+                self.is_pressed = False
+                self.myentery.delete(0,END)
 
-	def mainloop(self):
-		self.root.mainloop()
+    def mainloop(self):
+        self.root.mainloop()
