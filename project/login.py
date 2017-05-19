11b25c9c30a67628494aa3b4c9599b3e57f50660
Your Name <you@example.com>
2017-05-19 17:59:44 +0300

    added login thread

--- project/login.py
+++ project/login.py
@@ -2,6 +2,7 @@ from Tkinter import *
 import sys
 import socket
 import subprocess
+from threading import Thread
 
 class main_frame:
     def __init__(self,root):
@@ -17,56 +18,60 @@ class main_frame:
         self.namelabel.grid()
         self.phoneentery = Entry(self.root)
         self.phoneentery.grid(row = 1,column=1,sticky = W)
-
         self.button = Button(root,text = "Submit",command = self.server_con)
         self.button.grid(row=2,columnspan = 2)
+        self.sock = None
+        self.flag = False
 
     def server_con(self,key = None):
-        ip = "10.0.0.18"
+        ip = "10.0.0.21"
         PORT =5004
         ADD = (ip,PORT)
-        sock = socket.socket()
-        sock.connect(ADD)
-        sock.send("phone-"+self.phoneentery.get())
-        self.help_num(sock)
+        self.sock = socket.socket()
+        self.sock.connect(ADD)
+        self.sock.send("phone-"+self.phoneentery.get())
+        self.help_num()
 
-    def help_num(self,sock):
+    def help_num(self):
         self.phoneentery.delete(0,END)
         self.namelabel['text'] = "Enter verification number :"
-        self.button.configure(text = "Submit", command = lambda: self.num_ver(sock))
-        self.root.bind("<Return>", lambda a: self.num_ver(sock))
+        self.button.configure(text = "Submit", command = self.num_ver)
+        self.root.bind("<Return>", self.num_ver)
 
-    def num_ver(self,sock, key = None):
-        sock.send("num-"+self.phoneentery.get())
-        self.prog(sock)
+    def num_ver(self, key = None):
+        self.sock.send("num-"+self.phoneentery.get())
+        self.prog(self.sock)
 
     def prog(self,sock):
         self.root.unbind("<Return>")
-        self.phoneentery.pack_forget()
-        self.button.pack_forget()
+        self.phoneentery.destroy()
+        self.button.destroy()
         self.namelabel['text'] = "PLEASE WAIT !!!"
-        self.Create_con(sock)
+        self.flag = True
 
-    def Create_con(self,sock):
-        while True:
+    def Create_con(self):
+            while True:
+                if self.flag:
+                    try:
+                         data = self.sock.recv(1024)
+                         print data
+                         break
+                    except:
+                        pass
             try:
-                 data = sock.recv(1024)
-                 print data
-                 break
-            except:
-                pass
-        try:
-            self.root.destroy()
-            if data.startswith("client"):
-                process = subprocess.call(['python','client.py', data[7:]], shell=True, stderr=subprocess.STDOUT,
-                                           stdout=subprocess.PIPE)
-                print "process started"
+                self.root.destroy()
+                if data.startswith("client"):
+                    process = subprocess.call(['python','client.py', data[7:]], shell=True, stderr=subprocess.STDOUT,
+                                               stdout=subprocess.PIPE)
+                    print "process started"
 
-            else:
-                process = subprocess.call(['python','server.py'], shell=True, stderr=subprocess.STDOUT,
-                                           stdout=subprocess.PIPE)
-        except:
-                print "Connection Lost"
+                else:
+                    process = subprocess.call(['python','server.py'], shell=True, stderr=subprocess.STDOUT,
+                                               stdout=subprocess.PIPE)
+            except:
+                    print "Connection Lost"
 root = Tk()
 mainroot = main_frame(root)
+Connection_Thread = Thread(target=mainroot.Create_con)
+Connection_Thread.start()
 root.mainloop()
